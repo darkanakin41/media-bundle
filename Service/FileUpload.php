3cf667dd6cb38a470c47fa65e1b2fb392c2de9b9
Pierre LEJEUNE <darkanakin41@gmail.com>
2019-02-09 21:02:11 +0100

    Correction d'un bug lié à la non alimentation de la clé

--- Service/FileUpload.php
+++ Service/FileUpload.php
@@ -102,7 +102,7 @@ class FileUpload
                     if(isset($values['min_width'])){
                         $data += ['minWidth' => $values['min_width']];
                     }
-                    $retour[] = $data;
+                    $retour[$key] = $data;
                 }
             }
         }
