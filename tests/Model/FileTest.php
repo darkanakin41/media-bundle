ca4bade4a9fc8879d121b9e611b26dfa46e0cd11
Pierre LEJEUNE <darkanakin41@gmail.com>
2019-12-14 12:34:27 +0100

    Implementation of darkanakin/core-bundle entity testing way

--- tests/Model/FileTest.php
+++ tests/Model/FileTest.php
@@ -3,59 +3,43 @@
 namespace Darkanakin41\MediaBundle\Tests\Model;
 
 
+use Darkanakin41\CoreBundle\Tests\Model\AbstractEntityTestCase;
 use Darkanakin41\MediaBundle\Model\File;
-use PHPUnit\Framework\TestCase;
 
-class StreamTest extends TestCase
+class StreamTest extends AbstractEntityTestCase
 {
 
-    public function testId(){
-        $entity = $this->getFile();
-        $this->assertNull($entity->getId());
+    public function notNullableFieldProvider()
+    {
+        return [
+            ['filename', 'toto'],
+            ['filetype', 'toto'],
+            ['filesize', 'toto'],
+            ['category', 'toto'],
+        ];
     }
 
-    public function testFilepath(){
-        $entity = $this->getFile();
-
-        $entity->setFilepath("toto");
-        $this->assertEquals("toto", $entity->getFilepath());
+    public function nullableFieldProvider()
+    {
+        return [
+            ['copyright', 'toto']
+        ];
     }
 
-    public function testFilename(){
-        $entity = $this->getFile();
-
-        $entity->setFilename("toto");
-        $this->assertEquals("toto", $entity->getFilename());
+    public function defaultValueProvider()
+    {
+        return [
+        ];
     }
 
-    public function testFiletype(){
-        $entity = $this->getFile();
-
-        $entity->setFiletype("toto");
-        $this->assertEquals("toto", $entity->getFiletype());
+    /**
+     * @return File
+     */
+    protected function getEntity()
+    {
+        return $this->getMockForAbstractClass(File::class);
     }
 
-    public function testFilesize(){
-        $entity = $this->getFile();
-
-        $entity->setFilesize("toto");
-        $this->assertEquals("toto", $entity->getFilesize());
-    }
-
-    public function testCategory(){
-        $entity = $this->getFile();
-
-        $entity->setCategory("toto");
-        $this->assertEquals("toto", $entity->getCategory());
-    }
-
-    public function testCopyright(){
-        $entity = $this->getFile();
-        $this->assertNull($entity->getCopyright());
-
-        $entity->setCopyright("toto");
-        $this->assertEquals("toto", $entity->getCopyright());
-    }
 
     public function testDate(){
         $entity = $this->getFile();
@@ -65,7 +49,6 @@ class StreamTest extends TestCase
         $format = "Y-m-d H:i:s";
 
         $this->assertEquals($now->format($format), $entity->getDate()->format($format));
-
     }
 
     /**
